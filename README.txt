616355ae05f9ba8c7b01dad2a0ea0bc5ff602b58
Salih <sevgicansalih@gmail.com>
2018-03-10 22:04:48 +0300

    last version

---
+++ README.txt
@@ -0,0 +1,14 @@
+This project written with Python 2.7
+
+In order to run the code only type:
+
+python main.py
+
+Requirements:
+
+You need to put Dataset folder under main directory(put where main.py exists).
+
+Code is written by the assumption of that you are giving all dataset, not partial. This is crucial
+for the health of code :D. Otherwise, it'll give you uncorrect results. (I assumed the docID's are consecutive starting from the first document.)
+
+
--- main.py
+++ main.py
@@ -8,18 +8,29 @@ from os import listdir
 
 directory = 'Dataset/'
 
-list = [] # holds every document by their index (i)
-
 def main():
+
+	"""
+	IF REQUIRED LISTS ARE DUMPED NO NEED TO READ IT FROM FILES AGAIN
+	"""
+
 	if not (os.path.exists('stemmedDictList.pickle') and os.path.exists('stemmedNormalList.pickle') and os.path.exists('positionalIndex.pickle')):
+
+		"""
+		FIND REUTERS TAGS, ASSUME THAT NEWIDs ARE CONSECUTIVE
+
+		WITH THIS REGEX WE CAN FIND ALL NEWS
+		"""
 
 		rex = re.compile(r'<REUTERS.*?>(.*?)</REUTERS>.*?',re.DOTALL)
 
 		news = []
 
-
+		"""
+		READ FILES IN DATASET DIRECTORY
+		"""
 		onlyfiles = [f for f in listdir('Dataset') if os.path.isfile(os.path.join('Dataset', f))]
-		print sorted(onlyfiles)
+		#print sorted(onlyfiles)
 
 		for file in sorted(onlyfiles):
 			if file.endswith('.sgm'):
@@ -43,37 +54,36 @@ def main():
 						news.append(' ')
 
 				f.close()
-		"""
-		i=0
-		while i != 22:
-			if i<10:
-				dir = directory + '00{}.sgm'.format(i)
-			else:
-				dir = directory + '0{}.sgm'.format(i)
-
-			f = open(dir,'r')
-			data = f.read()
-			print 'Reading file : ',i
-			i = i+1
-			
-			#Find reuter tags (new id) in a single sgm file
-			reutersMatch = rex.findall(data)
-			for j in range(len(reutersMatch)):
-				titleMatch = getTitleMatch(reutersMatch[j])
-				bodyMatch = getBodyMatch(reutersMatch[j])
-				
-				if len(titleMatch)==1 and len(bodyMatch)==1:
-					news.append(titleMatch[0] + " " + bodyMatch[0])
-				elif len(titleMatch)==1 and len(bodyMatch)==0:
-					news.append(titleMatch[0])
-				elif len(titleMatch)==0 and len(bodyMatch)==1:
-					news.append(bodyMatch[0])
-				else:
-					news.append(' ')
-
-			f.close()
-		"""
+	
 		# now we have parsed data according to their newid-1 for their index
+
+		#TOKENIZER
+		tempNewsTokens = tokenize(news)
+
+
+		# CALCULATES NUMBER OF UNIQUE TOKENS BEFORE PROCESSING
+		numOfUniqueTokensBefore = 0
+		tempDict = {}
+		count = 0
+		for new in tempNewsTokens:
+			for token in new:
+				if  token in tempDict:
+					count = tempDict[token]
+					tempDict[token] = count+1
+				else:
+					tempDict[token] = 1
+		numOfUniqueTokensBefore = len(tempDict)
+
+		print 'Number of UNIQUE tokens before stopword removal and stemming', numOfUniqueTokensBefore
+
+		i = 0
+		print 'Frequency of the dictionary keys before stopword removal'
+		for w in sorted(tempDict, key=tempDict.get, reverse=True):
+			if i < 20:
+				print w, tempDict[w]
+			else:
+				break
+			i = i+1
 
 
 		# does case folding and normalizations
@@ -82,17 +92,39 @@ def main():
 		removePuncs(news)
 		print 'Removing punctiations DONE'
 
+
+
 		newsTokens = tokenize(news)
-
 		print 'Tokenizing DONE'
+		# CALCULATES UNIQUE TOKEN NUMBER BEFORE NORMALIZATIONS		
+		numOfUniqueTokensBefore = 0
+		tempDict = {}
+		count = 0
+	
+		for new in newsTokens:
+			for token in new:
+				tempDict[token] = count
+				count = count+1
+		numOfUniqueTokensBefore = len(tempDict)
 
+		# CALCULATES TOKEN NUMBER BEFORE NORMALIZATIONS
+		numOfTokensBefore = 0
+		for new in newsTokens:
+			numOfTokensBefore = numOfTokensBefore + len(new)
+
+		print 'Number of tokens before stopword removal and stemming', numOfTokensBefore
+
+		# STOPWORD REMOVAL
 		dictList,normalList = stopwordRemoval(newsTokens)
 
 		print 'Stopword Removal DONE'
 
 
-	#print dictList[21577] , len(dictList[21577])
 
+
+	"""
+	THIS PART DUMPS CALCULATED DATA BY USING PICKLE, IF FILES EXIST, IT IMPORTS THEM
+	"""
 	if os.path.exists('stemmedDictList.pickle'):
 		# this method gets list from a file 
 		stemmedDictList = getDumpedFile('stemmedDictList.pickle')
@@ -107,6 +139,8 @@ def main():
 	print 'Stemmed as dictionary DONE'
 
 	
+	
+
 	if os.path.exists('stemmedNormalList.pickle'):
 		# this method gets list from a file 
 		stemmedNormalList = getDumpedFile('stemmedNormalList.pickle')
@@ -114,12 +148,28 @@ def main():
 
 	else:
 		# this method calculates list but it takes so long
-		stemmedNormalList = stemizeList(normalList)
+		stemmedNormalList,fuckingRetarded = stemizeList(normalList)
 		dumpFile(stemmedNormalList,'stemmedNormalList.pickle')
 		print 'stemmedNormalList dumped'
+		numOfUniqueTokensAfter = len(fuckingRetarded)
+		print 'Number of UNIQUE tokens after stopword removal and stemming', numOfUniqueTokensAfter
+		i = 0 
+		print 'Frequency of the dictionary keys after stopword removal'
+		for w in sorted(fuckingRetarded, key=fuckingRetarded.get, reverse=True):
+			if i<20:
+				print w, fuckingRetarded[w]
+			else:
+				break
+			i = i+1	
 
 	print 'Stemmed as list DONE'
 	
+	# CALCULATES NUMBER OF TOKENS AFTER NORMALIZATION
+	numOfTokensAfter = 0
+	for aList in stemmedNormalList:
+		numOfTokensAfter = len(aList) + numOfTokensAfter
+	print 'Number of tokens after stopword removal and stemming', numOfTokensAfter
+
 
 	if os.path.exists('positionalIndex.pickle'):
 		# this method gets list from a file 
@@ -133,17 +183,22 @@ def main():
 		print 'posIndex Dumped'
 	print 'Positional Indexing DONE'
 
+
+	# PASS DATA TO QUERY PROCESSOR
 	queryProcessor(stemmedDictList,stemmedNormalList,posIndex)
 
 
 def queryProcessor(stemmedDictList,stemmedNormalList,posIndex):
+	# GETS DATA, WAITS FOR USER INPUT, AND PASSES INFORMATION TO SPECIFIED PROCESSORS
 	while True:
 		filename = raw_input('Enter query : ')
 		queryTokens = filename.split()
+
+		# QUERY NORMALIZATIONS
 		caseFolding(queryTokens)
 		removePuncs(queryTokens)
 		query = stemizeQuery(queryTokens)
-		print query
+		#print query
 		queryType = query[0]
 		if queryType == '1':
 			booleanQueryProcessor(query,posIndex)
@@ -153,6 +208,16 @@ def queryProcessor(stemmedDictList,stemmedNormalList,posIndex):
 			proximityQueryProcessor(query,posIndex,3)
 
 def proximityQueryProcessor(query,posIndex,type):
+	"""
+	TYPE 2 IS FOR PHRASE QUERIES
+
+	TYPE 3 IS FOR PROXIMITY QUERIES
+
+	HOLDS TERMS IN QUERY, AND CREATES A LIST OF PROXIMITY OF TERMS
+	IF THIS IS PHRASE QUERY, PROXIMITY LIST WILL BE FILLED WITH ZEROs
+
+	"""
+
 	terms = []
 	proximity = []
 	for index,token in enumerate(query):
@@ -169,16 +234,31 @@ def proximityQueryProcessor(query,posIndex,type):
 					proximity.append(int(token[1]))
 	if type==2:
 		del proximity[-1]
-	result = []
+	
+	"""
+	THIS PART DOES NORMAL INTERSECTION PROCESS AS IT IS LIKE IN BOOLEAN QUERY PROCESSOR
+
+	CREATES LIST OF COMMON DOCIDs
+	"""
 	termIndexes = []
 	for term in terms:
 		termIndexes.append(posIndex[term].keys())
 	docIDset = intersect(termIndexes)
 	docIDList = sorted(docIDset)
+
+
 	# now we know the docID's in which all terms are existed.
 	checkPositions(docIDList, terms, proximity,posIndex)
 
 def checkPositions(docIDList,terms,proximity,posIndex):
+	"""
+	THIS PART ITERATES OVER DOCIDs
+	AFTER WE ITERATE OVER QUERY TERMS
+	STARTING FROM INDEX# = 1 (NOT ZERO) CONTROLS TERMS POSITIONS UNDER SPECIFIED DOCID TWO BY TWO i.e. term0 and term1, term3 and term4
+
+	IN EVERY ITERATION IF THE NEXT TERM EXISTS IN GIVEN PROXIMITY, IT WILL BE ADDED TO RESULT LIST
+
+	"""
 	legth = len(terms)
 	result = []
 	for index, docID in enumerate(docIDList):
@@ -212,10 +292,18 @@ def checkPositions(docIDList,terms,proximity,posIndex):
 		
 		if len(resultPosition)!=0:
 			result.append(docID)
-	print len(result)	
-	print result
+	
+	print len(result)
+	# SINCE MY DOCIDs STARTS FROM ZERO, I ADD ONE
+	newResult = []
+	for res in result:
+		newResult.append(res+1)
+	print newResult
 
 def booleanQueryProcessor(query,posIndex):
+	"""
+	GETS QUERY TERMS, GETS POSITIONAL INDEX, INTERSECTS DOCIDs OF TERMS AND ADDS TO THE RESULT REGARDLESS OF TERM'S POSITION
+	"""
 	terms = []
 	for index,token in enumerate(query):
 		if index == 0:
@@ -227,11 +315,17 @@ def booleanQueryProcessor(query,posIndex):
 	termIndexes = []
 	for term in terms:
 		termIndexes.append(posIndex[term].keys())
-	print len(termIndexes)
+	#print len(termIndexes)
 	result = intersect(termIndexes)
-	print sorted(result)
+	newResult = []
+	for res in sorted(result):
+		newResult.append(res+1)
+	print newResult
 
 def intersect(termIndexes):
+	"""
+	BY USING SETS WE CAN FIND COMMON ELEMENTS IN A LIST
+	"""
 	result = []
 	sa = set(termIndexes[0])
 	sb = set(termIndexes[1])
@@ -246,43 +340,20 @@ def intersect(termIndexes):
 	return result
 
 
-
-def positionalIntersect(p1,p2,k,posIndex):
-	
-
-	"""
-	#p1 and p2 are token's dictionary
-	temp1 = p1.copy()
-	temp2 = p2.copy()
-	answer = []
-	for docID1, posList1 in temp1:
-		for docID2, posList2 in temp2:
-			if docID1 == docID2:
-				aList = []
-				for position1 in posList1:
-					for position2 in posList2:
-						if abs(position1 - position2) <= k:
-							aList.append(position2)
-						elif position2 > position1:
-							break;
-
-	"""
-
-
-
-
 def dumpFile(data,filename):
+	# PICKLE DUMPING FILE
 	with open(filename, 'wb') as handle:
 		pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 def getDumpedFile(filename):
+	# PICKLE LOADING FILE FROM DIRECTORY
 	with open(filename, 'rb') as handle:
 		data = pickle.load(handle)
 		return data
 def getStemmedNormalList():
+	# A METHOD FOR TESTING NOT REQUIRED FOR PROJECT
 	f = open('stemmedNormalList.txt','r')
 	data = f.read()
-	
 	stemmedNormalList = []
 	newIDs = data.split('#')
 	for new in newIDs:
@@ -293,6 +364,12 @@ def getStemmedNormalList():
 		stemmedNormalList.append(aList)
 	return stemmedNormalList
 def positionalIndexer(stemmedDictList,stemmedNormalList):
+	"""
+	{terms:{docIDs : [positions]}}
+
+	EXACTLY DOES THE THING SHOWN ABOVE
+
+	"""
 	# a list which holds token id's as index ( all token id's are unique), list member holds a dict, dict key is newID dict value is position in that newID 
 	tokenDict = {}
 
@@ -313,59 +390,39 @@ def positionalIndexer(stemmedDictList,stemmedNormalList):
 				else:
 					tokenDict[token][indexDoc].append(indexToken)
 			
-	"""
-	aList = stemmedNormalList[0]
-	
-	indexDoc = stemmedNormalList.index(aList)
-	print 'in another list' , indexDoc
-	for indexToken,token in enumerate(aList):
-
-		docIDDict = {}
-		positions = []
-
-		print id(positions), " id"
-		if not token in tokenDict:
-			print "token not in dict"
-			positions.append(indexToken)
-			docIDDict[indexDoc] = positions
-			tokenDict[token] = docIDDict
-		else:
-			if not indexDoc in tokenDict[token]:
-				print "token is in dict but no docDict for this docID"
-				positions.append(indexToken)
-				tokenDict[token][indexDoc] = positions 
-			else:
-				print "token is in dict ,position list updated for this docID dict"
-				tokenDict[token][indexDoc].append(indexToken)
-
-	print "this is a list"
-	print aList
-	"""
-	#print "this is token dict"
-	#print tokenDict	
 	
 	return tokenDict
 
 def stemize(dictList):
+	#STEMIZER FOR DICTIONARY
 	porter = PorterStemmer()		
 	for dict in dictList:
 		for token in dict.keys():
-			dict[porter.stem(token,0,len(token)-1)] = dict.pop(token)
+			dict[porter.stem(token,0,len(token)-1)] = dict.pop(token) 
 	return dictList
 
 def stemizeList(normalList):
+	# STEMIZER FOR LIST
 	porter = PorterStemmer()
 	newList = []
+	newDict = {}
+	count = 0;	
 	for lists in normalList:
 		tokenList = []
 		for token in lists:
 			#print normalList.index(lists)," ",lists.index(token)
 			tokenList.append(porter.stem(token,0,len(token)-1))
+			if token in newDict:
+				count = newDict[token]
+				newDict[token] = count +1
+			else:
+				newDict[token] = 1
 		newList.append(tokenList)
 			#token = porter.stem(token,0,len(token)-1)
 			
-	return newList
+	return newList,newDict
 def stemizeQuery(query):
+	# STEMIZER FOR QUERY
 	porter = PorterStemmer()
 	newList = []
 	for q in query:
@@ -373,6 +430,8 @@ def stemizeQuery(query):
 	return newList
 
 def tokenize(news):
+	# TOKENIZER LIST OF NEWS
+
 	list = []
 	for new in news:
 		list.append(new.split())
@@ -381,6 +440,7 @@ def tokenize(news):
 	return list
 
 def stopwordRemoval(newsTokens):
+	# GETS STOPWORDS FROM STOPWORD FILE, IF EXISTS IT REMOVES THEM
 	dictList = []
 	normalList = []
 	f = open('stopwords.txt','r')
@@ -399,16 +459,20 @@ def stopwordRemoval(newsTokens):
 		normalList.append(aList)
 	return dictList,normalList
 
+
 def removePuncs(news):
+	# REMOVING PUNCTIATIONS FROM NEWS LIST
 	for i in range(len(news)):
 		news[i] = re.sub(r'[^a-zA-Z0-9]', ' ', news[i]) 
 
 	
 def caseFolding(news):
+	# LOWERS NEWS
 	for i in range(len(news)):
 		news[i] = news[i].lower()
 
 def getTitleMatch(data):
+	# GETS TITLE INSIDE OF REUTERS TAG
 	rex = re.compile(r'<TITLE>(.*?)</TITLE>',re.DOTALL)
 	titleMatch = rex.findall(data)
 	#print "Title length is : ",len(titleMatch)
@@ -417,6 +481,7 @@ def getTitleMatch(data):
 
 
 def getBodyMatch(data):
+	# GETS BODY TEXT INSIDE OF REUTERS TAG
 	rex = re.compile(r'<BODY>(.*?)</BODY>',re.DOTALL)
 	bodyMatch = rex.findall(data)
 	#print "Body length is : ",len(bodyMatch)
